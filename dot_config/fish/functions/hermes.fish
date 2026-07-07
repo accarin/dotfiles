b14f70392d4bc3a566df88b6667ef0a24420adb3
Marc 'Accarin' Tautschnig <marc@tautschnig.org>
2026-07-07 23:03:47 +0200

    Add hermes function to run Hermes Agent via Docker

---
+++ dot_config/fish/functions/hermes.fish
@@ -0,0 +1,15 @@
+function hermes --description "Run Hermes Agent via Docker"
+    docker run -it --rm \
+        --network ornith-hermes-stack_default \
+        -v ~/.hermes:/opt/data \
+        -v ~/ornith-hermes-stack/projects:/workspace \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        -w /workspace \
+        -e HERMES_UID=(id -u) \
+        -e HERMES_GID=(id -g) \
+        -e HERMES_LLM_BASE_URL=http://ornith-llm:8080/v1 \
+        -e HERMES_API_KEY=local \
+        -e HERMES_MODEL=ornith-35b \
+        nousresearch/hermes-agent \
+        hermes $argv
+end
