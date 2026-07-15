b406c5a239612853e5229d9399fdfe68a9a81065
Marc 'Accarin' Tautschnig <marc@tautschnig.org>
2026-07-15 13:51:31 +0200

    Enhance hermes function by adding environment variables for HOME, HERMES_WRITE_SAFE_ROOT, and HERMES_ACCEPT_HOOKS

--- dot_config/fish/functions/hermes.fish
+++ dot_config/fish/functions/hermes.fish
@@ -5,11 +5,14 @@ function hermes --description "Run Hermes Agent via Docker"
         -v ~/ornith-hermes-stack/projects:/workspace \
         -v /var/run/docker.sock:/var/run/docker.sock \
         -w /workspace \
+        -e HOME=/workspace \
+        -e HERMES_WRITE_SAFE_ROOT=/workspace \
         -e HERMES_UID=(id -u) \
         -e HERMES_GID=(id -g) \
         -e HERMES_LLM_BASE_URL=http://ornith-llm:8080/v1 \
         -e HERMES_API_KEY=local \
         -e HERMES_MODEL=ornith-35b \
+        -e HERMES_ACCEPT_HOOKS=1 \
         nousresearch/hermes-agent \
         hermes $argv
 end
